752ee641aaf802c61b990b24f8f949571f5a69d9
montazeri_a <montazeri_a@office.zharfpouyan.net>
2022-10-11 13:54:27 +0330

    ..

---
+++ app/Http/Controllers/Auth/AuthAuthController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AuthRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthAuthController extends Controller
+{
+    public function login(AuthRequest $request)
+    {
+        if(Auth::attempt([
+            'username' => $request->input('username'), 
+            'password' => $request->input('password'), 
+        ])) 
+        {
+            $user = $request->user();
+            $tokenResult = $user->createToken('Api Token');
+            $token = $tokenResult->token;
+
+            if($token->save())
+                return response()->json([
+                    'state' => true,
+                    'message' => __('auth.loggedIn'), 
+                    'data' => [
+                        'avatar' => $user->avatar,
+                        'firstName' => $user->first_name,
+                        'lastName' => $user->last_name,
+                        'email' => $user->email,
+                        'token' => $tokenResult->accessToken,
+                        'token_type' => 'Bearer', 
+                        'userName' => $user->username,
+                    ]
+                ], 200);
+        }
+        return response()->json([
+            'state' => true,
+            'message' => __('auth.incorrect'),
+            'data' => null
+        ], 403);
+    }
+    
+    public function register(AuthRequest $request)
+    {
+        $user = new User();
+        $user->first_name = $request->input('first_name');
+        $user->last_name = $request->input('last_name');
+        $user->username = $request->input('username');
+        $user->email = $request->input('email');
+        $user->mobile = $request->input('mobile');
+        $user->postal_code = $request->input('postal_code');
+        $user->home_address = $request->input('home_address');
+        $user->work_address = $request->input('work_address');
+        $user->password = Hash::make($request->input('password'));
+        if ($user->save())
+            return response()->json([
+                'state' => true,
+                'message' => __('auth.register'),
+                'data' => null,
+            ], 200);
+
+        return response()->json([
+            'state' => false,
+            'message' => __('general.againLater'),
+            'data' => null,
+        ], 500);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+        return response()->json([
+            'state' => true,
+            'message' => __('auth.loggedOut'),
+            'data' => null,
+        ], 200);
+    }
+
+}
---
+++ resources/lang/fa/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'incorrect' => 'نام کاربری یا رمز عبور اشتباه است',
+    'loggedIn' => 'شما وارد شده اید',
+    'loggedOut' => 'شما با موفقیت از سیستم خارج شدید',
+    'register' => 'با تشکر، شما با موفقیت ثبت نام کردید. لطفا ایمیل خود را تایید کنید...'
+];
